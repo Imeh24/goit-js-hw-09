5c518d34c5432feac5dcb49c93f91ceb77a4ea7c
Imeh24 <136251757+Imeh24@users.noreply.github.com>
2024-01-04 02:11:09 +0000

    Deploying to gh-pages from @ Imeh24/goit-js-hw-09@6d76fde876cbb6b817bd0a717bbe9a29f050d1b0 🚀

---
+++ 01-color-switcher.feffd497.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]");function o(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}let n=null,d="";t.addEventListener("click",(()=>{t.disabled=!0,n=setInterval((()=>{d=o(),document.body.style.backgroundColor=o()}),1e3)})),e.addEventListener("click",(()=>{clearInterval(n),document.body.style.backgroundColor=d,t.disabled=!1}));
+//# sourceMappingURL=01-color-switcher.feffd497.js.map
