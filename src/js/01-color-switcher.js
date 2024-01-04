0becf3d8e0218fc7abe5dd43924ae47c1deca7d5
Imeh24 <medinajeremymarcel@gmail.com>
2024-01-04 09:45:33 +0800

    code for task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,22 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
 
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  } 
+
+  let interval = null;
+startButton.addEventListener('click', () => {
+    startButton.disabled = true;
+
+    interval = setInterval(() => {
+        document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+}); 
+
+stopButton.addEventListener('click', () => {
+    clearInterval(interval);
+    document.body.style.backgroundColor = '';
+    startButton.disabled = false;
+});
